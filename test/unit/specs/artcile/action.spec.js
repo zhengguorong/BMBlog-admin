fac7dd41edda3d444117b051816a98b69b9465b9
郑国榕 <zhengguorong@bluemoon.com.cn>
2016-12-06 12:22:18 +0800

    添加user单元测试

--- test/unit/specs/artcile/action.spec.js
+++ test/unit/specs/artcile/action.spec.js
@@ -1,8 +1,8 @@
 /**
  * Created by zhengguorong on 2016/11/30.
  */
-import { expect } from 'chai'
 import Article from 'src/model/Article'
+import tools from '../../tools'
 const actionsInjector = require('inject!src/vuex/article/actions')
 
 const article = new Article()
@@ -36,63 +36,38 @@ const actions = actionsInjector({
   }
 })
 
-// 用指定的 mutaions 测试 action 的辅助函数
-const testAction = (action, args, state, expectedMutations, done) => {
-  let count = 0
-  // 模拟提交
-  const commit = (type, payload) => {
-    const mutation = expectedMutations[count]
-    expect(mutation.type).to.equal(type)
-    if (payload) {
-      expect(mutation.payload).to.deep.equal(payload)
-    }
-    count++
-    if (count >= expectedMutations.length) {
-      done()
-    }
-  }
-  // 用模拟的 store 和参数调用 action
-  action({ commit, state }, ...args)
-
-  // 检查是否没有 mutation 被 dispatch
-  if (expectedMutations.length === 0) {
-    expect(count).to.equal(0)
-    done()
-  }
-}
-
 describe('article actions', () => {
   it('获取文章列表不为空', done => {
-    testAction(actions.getArticleList, [], {}, [
+    tools.testAction(actions.getArticleList, [], {}, [
       { type: 'GET_ARTICLE_LIST', payload: articleList },
       { type: 'SET_EDITOR_ARTICLE', payload: articleList[0] }
     ], done)
   })
   it('获取文章列表为空', done => {
     articleList = []
-    testAction(actions.getArticleList, [], {}, [
+    tools.testAction(actions.getArticleList, [], {}, [
       { type: 'SET_EDITOR_ARTICLE', payload: article }
     ], done)
   })
   it('添加文章', done => {
-    testAction(actions.addArticle, [], {}, [
+    tools.testAction(actions.addArticle, [], {}, [
       { type: 'ADD_ARTICLE', payload: article }
     ], done)
   })
   it('设置正在编辑的文章', done => {
-    testAction(actions.setEditorArticle, [], {}, [
+    tools.testAction(actions.setEditorArticle, [], {}, [
       { type: 'SET_EDITOR_ARTICLE', payload: article }
     ], done)
   })
   it('保存文章', done => {
-    testAction(actions.saveArticle, [], {}, [
+    tools.testAction(actions.saveArticle, [], {}, [
       { type: 'CREATE_ARTICLE_SUCCESS', payload: '' }
     ], done)
   })
   it('更新文章', done => {
     var updateArticle = article
     updateArticle._id = '1234567'
-    testAction(actions.saveArticle, [updateArticle], {}, [
+    tools.testAction(actions.saveArticle, [updateArticle], {}, [
       { type: 'UPDATE_ARTICLE_SUCCESS', payload: '' }
     ], done)
   })
