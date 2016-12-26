ab56e561a59dda1efe3612fe1996dcd240cfe77c
郑国榕 <zhengguorong@bluemoon.com.cn>
2016-12-26 16:45:00 +0800

    加入取消元素选择

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class="[type!=='see'?'canvas':'']">
+  <div :class="[type!=='see'?'canvas':'']"  @click="cleanSelected">
     <div>
       <template v-for="element in elements">
-        <div @click="selectedElement(element)">
+        <div @click.stop="selectedElement(element)">
           <PicElement :type="type" v-if="element.type==='pic'" :class="[element.playing?'animated ' + element.animatedName:'']" :element="element"
             :style="{transform:'rotate('+element.transform+'deg)',opacity:element.opacity/100,width:element.width+'px',height:element.height+'px',top:element.top+'px',left:element.left + 'px','animation-duration':element.duration + 's','-webkit-animation-duration':element.duration + 's','animation-delay':element.delay + 's','-webkit-animation-delay':element.delay + 's'}"
             :showOperate="editorElement == element">
@@ -53,6 +53,10 @@
     methods: {
       selectedElement (element) {
         this.$store.dispatch('setEditorElement', element)
+      },
+      cleanSelected () {
+        console.log('chean')
+        this.$store.dispatch('setEditorElement', {})
       }
     },
     components: {
