c7e9bbbd37d3a4eefa5149d4cf5aa7d1791318ab
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-05-19 15:49:00 +0800

    修复描述显示问题

--- src/views/h5editor/index.vue
+++ src/views/h5editor/index.vue
@@ -183,9 +183,7 @@
         dialogSaveBeforeBack: false,
         animateList: ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'bounceOut', 'bounceOutDown', 'bounceOutLeft', 'bounceOutRight', 'bounceOutUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'fadeOut', 'fadeOutDown', 'fadeOutDownBig', 'fadeOutLeft', 'fadeOutLeftBig', 'fadeOutRight', 'fadeOutRightBig', 'fadeOutUp', 'fadeOutUpBig', 'flip', 'flipInX', 'flipInY', 'flipOutX', 'flipOutY', 'lightSpeedIn', 'lightSpeedOut', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'rotateOut', 'rotateOutDownLeft', 'rotateOutDownRight', 'rotateOutUpLeft', 'rotateOutUpRight', 'slideInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideOutUp', 'slideOutDown', 'slideOutLeft', 'slideOutRight', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'zoomOut', 'zoomOutDown', 'zoomOutLeft', 'zoomOutRight', 'zoomOutUp', 'hinge', 'rollIn', 'rollOut'],
         picBase64: '',
-        http: appConst.BACKEND_DOMAIN,
-        title: this.$store.state.editor.editorTheme.title || '',
-        description: this.$store.state.editor.editorTheme.description || ''
+        http: appConst.BACKEND_DOMAIN
       }
     },
     watch: {
@@ -221,6 +219,12 @@
       },
       picList () {
         return this.$store.state.editor.picList
+      },
+      title () {
+        return this.$store.state.editor.editorTheme.title || ''
+      },
+      description () {
+        return this.$store.state.editor.editorTheme.description || ''
       }
     },
     methods: {
